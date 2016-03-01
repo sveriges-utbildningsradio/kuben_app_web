043a0e84c55758943b6595f574520f875caf30cd
jan.igerud <jan.igerud>
2016-03-01 20:00:28 +0100

    changed size of Save button #2

--- Javascript/javascriptInjectCode.js
+++ Javascript/javascriptInjectCode.js
@@ -344,7 +344,6 @@ var UR = new function() {
         bookmarkButton.style.padding = '12px 24px';
         bookmarkButton.style.fontFamily = 'Open Sans';
         bookmarkButton.style.borderRadius = '2px';
-        bookmarkButton.style.height ='43px';
         bookmarkButton.style.width ='74px';
         bookmarkButton.style.backgroundSize = "8px 12px";
         bookmarkButton.style.fontSize = '1.4rem';
