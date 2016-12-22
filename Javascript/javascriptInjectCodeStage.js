26f66693c31000bf163b9e63dc465cbe384a0738
Petter Stenberg <pst@ur.se>
2016-12-22 16:50:09 +0100

    [APPAND-137]
    - Did a few css adjustments of the the cast text.

--- Javascript/javascriptInjectCodeStage.js
+++ Javascript/javascriptInjectCodeStage.js
@@ -442,8 +442,8 @@ var UR = new function() {
             castText.style.transform = 'translate(-50%, -50%)';
             castText.style.color = '#FFFFFF';
             castText.style.textAlign = 'center';
-            castText.style.lineHeight = '1.2em';
-            castText.style.fontSize = '1.8em';
+            castText.style.lineHeight = '1.5em';
+            castText.style.fontSize = '1.7em';
             castText.innerHTML = "Sänder till<Br />" + castDevice;
             containers[0].appendChild(castText);
         }
