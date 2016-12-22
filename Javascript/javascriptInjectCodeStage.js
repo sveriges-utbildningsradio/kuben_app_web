21ec426c169935ee70e6d61b11c7ddbdcb42fb46
Petter Stenberg <pst@ur.se>
2016-12-22 18:28:05 +0100

    [APPAND-137]
    - Did a few css adjustments of the the cast text.

--- Javascript/javascriptInjectCodeStage.js
+++ Javascript/javascriptInjectCodeStage.js
@@ -687,14 +687,14 @@ var UR = new function() {
         }
     };
 
-    function setCookie(cname, cvalue, exdays) {
+    this.setCookie = function (cname, cvalue, exdays) {
         var d = new Date();
         d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
         var expires = "expires="+d.toUTCString();
         document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
     }
 
-    function getCookie(cname) {
+    this.getCookie = function (cname) {
         var name = cname + "=";
         var ca = document.cookie.split(';');
         for(var i = 0; i < ca.length; i++) {
